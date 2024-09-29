2ff05247e67567e0f872dafe267ad04cc591e1e5
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2024-09-29 13:32:55 +0300

    Update settings.py

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -1,26 +1,26 @@
 from pathlib import Path
 
 BOT_NAME = 'pep_parse'
-
-SPIDER_MODULES = ['pep_parse.spiders']
 NEWSPIDER_MODULE = 'pep_parse.spiders'
-
+SPIDER_MODULES = [NEWSPIDER_MODULE]
 ROBOTSTXT_OBEY = True
 
+PEP_DOMAIN = 'peps.python.org'
+
+BASE_DIR = Path(__file__).parent.parent
+RESULTS_DIR = 'results'
+PEP_OUTPUT_FILE = 'pep_%(time)s.csv'
+STATUS_SUMMARY_OUTPUT_FILE = 'status_summary_{now_formatted}.csv'
+
+FILE_DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+
 FEEDS = {
-    'results/pep_%(time)s.csv': {
+    f'{RESULTS_DIR}/{PEP_OUTPUT_FILE}': {
         'format': 'csv',
-        'fields': ['number', 'name', 'status'],
-        'overwrite': True
+        'fields': ['number', 'name', 'status']
     },
 }
 
 ITEM_PIPELINES = {
     'pep_parse.pipelines.PepParsePipeline': 300,
 }
-
-BASE_DIR = Path(__file__).parent.parent
-RESULTS_DIR = 'results'
-STATUS_SUMMARY_OUTPUT_FILE = 'status_summary_{now_formatted}.csv'
-
-FILE_DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
